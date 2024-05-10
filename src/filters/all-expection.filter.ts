5cbd6c2f129506397e4dd8eab535a404dc710f76
OceanH <1404939185@qq.com>
2024-05-10 15:24:19 +0800

    feat: 配置请求参数的自动验证并处理通用错误信息过滤器的冲突

--- src/filters/all-expection.filter.ts
+++ src/filters/all-expection.filter.ts
@@ -12,23 +12,35 @@ import { Request, Response } from 'express';
 import * as moment from 'moment';
 
 // 负责捕获HttpException类的实例
-@Catch(HttpException)
-export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const request = ctx.getRequest<Request>();
     const response = ctx.getResponse<Response>();
-    const status = exception.getStatus();
 
-    // 用于接收主动报错的错误信息
-    const { message } = exception;
-    response.status(status).json({
-      status,
-      path: request.url,
-      err: 'Bad Request',
-      method: request.method,
-      message,
-      timeStamp: moment().format('yyyy-MM-DD HH:mm:ss'),
-    });
+    if (exception instanceof HttpException && exception.getStatus() === 400) {
+      // 提取错误信息
+      const errors = (exception.getResponse() as any).message || [];
+
+      response.status(400).json({
+        statusCode: 400,
+        timestamp: moment().format('yyyy-MM-DD HH:mm:ss'),
+        message: 'Validation failed',
+        errors, // 返回具体的验证错误详情
+      });
+    } else {
+      // 对于其他类型的异常，就返回通用的错误信息
+      response
+        .status(
+          exception instanceof HttpException ? exception.getStatus() : 500,
+        )
+        .json({
+          statusCode:
+            exception instanceof HttpException ? exception.getStatus() : 500,
+          timestamp: moment().format('yyyy-MM-DD HH:mm:ss'),
+          message: (exception as any).message || 'Internal server error',
+        });
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptors } from './interceptors/transform.interceptors';
-import { HttpExceptionFilter } from './filters/all-expection.filter';
+import { AllExceptionsFilter } from './filters/all-expection.filter';
 
 // session
 import * as session from 'express-session';
+
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +15,7 @@ async function bootstrap() {
   // 全局注册拦截器
   app.useGlobalInterceptors(new TransformInterceptors());
   // 全局注册错误过滤器
-  app.useGlobalFilters(new HttpExceptionFilter());
+  // app.useGlobalFilters(new HttpExceptionFilter());
   // 定义Session
   app.use(
     session({
@@ -23,6 +25,17 @@ async function bootstrap() {
       rolling: true,
     }),
   );
+  // 全局配置 ValidationPipe进行自动验证
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // 这里可以设置 ValidationPipe 的配置选项
+      whitelist: true, // 是否只保留 DTO 中定义的属性
+      forbidNonWhitelisted: true, // 是否禁止非 DTO 中定义的属性
+      // 其他配置...
+    }),
+  );
+  // 全局注册错误过滤器
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(3000);
 }
 bootstrap();
